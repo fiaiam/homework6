4eb6b1eaaf0f2cedc9b626be7047a98c7baec448
fiaiam <fiaiam@yandex.ru>
2024-07-14 15:21:08 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,25 @@
+#Работа со словарями
+my_dict = {'Masha': 2003, 'Dina': 1999, 'Sveta': 1985}
+print('Dict: ', my_dict)
+#По существующему:
+print('Existing value: ',my_dict['Masha'])
+#По отсутствующему:
+print(my_dict.get('Anton','Not existing value: None'))
+#Добавление пар
+my_dict.update({'Ira': 1980,
+                'Sonya': 2001})
+#Удалить одну из пар и вывести значение
+a = my_dict.pop('Sveta')
+print('Deleted value: ', a)
+#Вывести на экран словарь
+print('Modified dictionary: ',my_dict)
+
+#Работа с множествами
+my_set = {20,23,20,24,28,25,23,20,21,20,21,23}
+print('Set: ', my_set)
+#Добавление 2-х произвольных элемента
+my_set.add(29)
+my_set.add(30)
+#Удаление элемента
+my_set.remove(21)
+print('Modified set: ', my_set)
